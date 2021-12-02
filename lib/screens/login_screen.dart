e0f4cfdb87cdd23bd371dd97aa30af4768ad02b6
Simone Auriemma <simone.auriemma@live.com>
2021-12-02 21:41:37 +0100

    added login firebase method with library

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pet360/screens/registration_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -14,6 +16,32 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
 
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     //email
     final emailFilds = TextFormField(
@@ -62,7 +90,19 @@ class _LoginScreenState extends State<LoginScreen> {
       child: MaterialButton(
         padding: EdgeInsets.fromLTRB(20, 15, 15, 10),
         minWidth: MediaQuery.of(context).size.width,
-        onPressed: () {},
+        onPressed: () async {
+          try {
+            UserCredential userCredential = await FirebaseAuth.instance
+                .signInWithEmailAndPassword(
+                    email: "test1@test.it", password: "test123");
+          } on FirebaseAuthException catch (e) {
+            if (e.code == 'user-not-found') {
+              print('No user found for that email.');
+            } else if (e.code == 'wrong-password') {
+              print('Wrong password provided for that user.');
+            }
+          }
+        },
         child: Text(
           "Login",
           textAlign: TextAlign.center,
@@ -81,51 +121,51 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Padding(
               padding: const EdgeInsets.all(36.0),
               child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 200,
-                      child: Image.asset(
-                          "assets/logoPet360.png",
-                          fit: BoxFit.contain,
-                      )),
-                    SizedBox(height: 45),
-
-                    emailFilds,
-                    SizedBox(height: 25),
-
-                    passwordFilds,
-                    SizedBox(height: 35),
-
-                    loginButton,
-                    SizedBox(height: 15),
-
-                    Row(
+                  key: _formKey,
+                  child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Text("Non hai un account? "),
-                        GestureDetector(onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => RegistrationScreen()));
-                        },
-                        child: Text(
-                          "Clicca qui!",
-                          style: TextStyle(
-                            color: Colors.lightGreen,
-                            fontWeight: FontWeight.w600, fontSize: 15),
-                          ),
-                        ),
-                      ],
-                    )
-                  ]
-                )
-                ),
-              ),
+                        SizedBox(
+                            height: 200,
+                            child: Image.asset(
+                              "assets/logoPet360.png",
+                              fit: BoxFit.contain,
+                            )),
+                        SizedBox(height: 45),
+                        emailFilds,
+                        SizedBox(height: 25),
+                        passwordFilds,
+                        SizedBox(height: 35),
+                        loginButton,
+                        SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("Non hai un account? "),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            RegistrationScreen()));
+                              },
+                              child: Text(
+                                "Clicca qui!",
+                                style: TextStyle(
+                                    color: Colors.lightGreen,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 15),
+                              ),
+                            ),
+                          ],
+                        )
+                      ])),
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 }
