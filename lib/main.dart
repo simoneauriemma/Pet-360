618f114f75c2d2b9dcb7f655bbde094732694bc7
Simona Grieco <simonagrieco98@gmail.com>
2021-12-03 17:02:40 +0100

    Modifiche login, registrazione e home page (RegExpr)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 //import 'package:pet360/screens/home_screen.dart';
 import 'package:pet360/screens/login_screen.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
