d4b6a4a9e3652ea1eecc48a7988e3d799d97090b
Simone Auriemma <simone.auriemma@live.com>
2021-12-07 23:35:20 +0100

    Added auto login

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pet360/screens/addnewanimal_screen.dart';
 import 'package:pet360/screens/chat_screen.dart';
@@ -6,6 +8,8 @@ import 'package:pet360/screens/profile_screen.dart';
 import 'dart:convert';
 import 'dashboard.dart';
 import 'package:http/http.dart' as http;
+
+import 'login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -26,6 +30,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   final PageStorageBucket bucket = PageStorageBucket();
   Widget currentScreen = Dashboard();
+  final _auth = FirebaseAuth.instance;
+
   var jsonBody;
 
   @override
@@ -40,14 +46,12 @@ class _HomeScreenState extends State<HomeScreen> {
         child: const Icon(Icons.add_outlined),
         onPressed: () {
           setState(() {
-            currentScreen= AddNewAnimalScreen();
-            currentTab=0;
-            });
+            currentScreen = AddNewAnimalScreen();
+            currentTab = 0;
+          });
         },
       ),
-
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
         notchMargin: 10,
@@ -61,30 +65,38 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   MaterialButton(
-                      minWidth: 40,
-                      onPressed: () {
-                        setState(() {
-                          currentScreen= Dashboard();
-                          currentTab=0;
-                        });
-                      },
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Dashboard();
+                        currentTab = 0;
+                      });
+                    },
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Icon(
                           Icons.home_filled,
-                          color: currentTab==0 ? Colors.lightGreen.shade300 : Colors.grey,
+                          color: currentTab == 0
+                              ? Colors.lightGreen.shade300
+                              : Colors.grey,
                         ),
-                        Text('Home', style: TextStyle(color: currentTab==0 ? Colors.lightGreen.shade300 : Colors.grey),)
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                              color: currentTab == 0
+                                  ? Colors.lightGreen.shade300
+                                  : Colors.grey),
+                        )
                       ],
                     ),
-                      ),
+                  ),
                   MaterialButton(
                     minWidth: 40,
                     onPressed: () {
                       setState(() {
-                        currentScreen= LocationScreen();
-                        currentTab=1;
+                        currentScreen = LocationScreen();
+                        currentTab = 1;
                       });
                     },
                     child: Column(
@@ -92,9 +104,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Icon(
                           Icons.location_on,
-                          color: currentTab==1 ? Colors.lightGreen.shade300 : Colors.grey,
+                          color: currentTab == 1
+                              ? Colors.lightGreen.shade300
+                              : Colors.grey,
                         ),
-                        Text('Traccia', style: TextStyle(color: currentTab==1? Colors.lightGreen.shade300 : Colors.grey),)
+                        Text(
+                          'Traccia',
+                          style: TextStyle(
+                              color: currentTab == 1
+                                  ? Colors.lightGreen.shade300
+                                  : Colors.grey),
+                        )
                       ],
                     ),
                   ),
@@ -107,8 +127,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     minWidth: 40,
                     onPressed: () {
                       setState(() {
-                        currentScreen= ChatScreen();
-                        currentTab=2;
+                        currentScreen = ChatScreen();
+                        currentTab = 2;
                       });
                     },
                     child: Column(
@@ -116,9 +136,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Icon(
                           Icons.chat_rounded,
-                          color: currentTab==2 ? Colors.lightGreen.shade300 : Colors.grey,
+                          color: currentTab == 2
+                              ? Colors.lightGreen.shade300
+                              : Colors.grey,
                         ),
-                        Text('Chat', style: TextStyle(color: currentTab==2 ? Colors.lightGreen.shade300 : Colors.grey),)
+                        Text(
+                          'Chat',
+                          style: TextStyle(
+                              color: currentTab == 2
+                                  ? Colors.lightGreen.shade300
+                                  : Colors.grey),
+                        )
                       ],
                     ),
                   ),
@@ -126,8 +154,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     minWidth: 40,
                     onPressed: () {
                       setState(() {
-                        currentScreen= LocationScreen();
-                        currentTab=3;
+                        currentScreen = LocationScreen();
+                        currentTab = 3;
                       });
                     },
                     child: Column(
@@ -135,9 +163,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Icon(
                           Icons.account_circle_rounded,
-                          color: currentTab==3 ? Colors.lightGreen.shade300 : Colors.grey,
+                          color: currentTab == 3
+                              ? Colors.lightGreen.shade300
+                              : Colors.grey,
                         ),
-                        Text('Profilo', style: TextStyle(color: currentTab==3 ? Colors.lightGreen.shade300 : Colors.grey),)
+                        Text(
+                          'Profilo',
+                          style: TextStyle(
+                              color: currentTab == 3
+                                  ? Colors.lightGreen.shade300
+                                  : Colors.grey),
+                        )
                       ],
                     ),
                   ),
@@ -147,16 +183,29 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-
     );
   }
+
+  void LogOut() async {
+    await _auth.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+
   //getData("User","uid","surnameName")
   getData(String typeOfUser, String uidUser, String path) async {
     var url = Uri.parse(
-        "https://pet360-43dfe-default-rtdb.europe-west1.firebasedatabase.app//" + typeOfUser + "//" + uidUser + "//" + path + ".json?");
+        "https://pet360-43dfe-default-rtdb.europe-west1.firebasedatabase.app//" +
+            typeOfUser +
+            "//" +
+            uidUser +
+            "//" +
+            path +
+            ".json?");
     var response = await http.get(url);
     if (response.statusCode == 200) {
       jsonBody = json.decode(response.body);
+      print(jsonBody['cityName']);
     } else {
       print('Request failed with status: ${response.statusCode}.');
     }
