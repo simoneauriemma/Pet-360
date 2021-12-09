6207350009faefe16f68052bf5e001c1d0e10ab1
Elio Testa <eliotesta1998@libero.it>
2021-12-09 23:37:58 +0100

    Profilo e Login
    Risolto Quary al profilo e Login

--- lib/utils/usersharedpreferences.dart
+++ lib/utils/usersharedpreferences.dart
@@ -6,6 +6,7 @@ class UserSharedPreferences {
 
   static Future<void> init() async {
     _preferences = await SharedPreferences.getInstance();
+    setTypeOfUser("");
   }
 
   static Future setTypeOfUser(String typeOfUser) async =>
