591673845458317b4fbeeb5a5205e6291eb421f1
Simone Auriemma <simone.auriemma@live.com>
2021-12-14 00:33:55 +0100

    added location page, with code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-o du# Pet-360
+# Pet-360
 Pet 360 ha come primo obiettivo il benessere degli animali domestici e quello del suo padrone. Quest’ultimo vuole sfruttare un semplice sistema che lo aiuti nella sua vita quotidiana insieme al suo cucciolo. 
 
 # Ambito del Sistema
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({Key? key}) : super(key: key);
@@ -8,6 +16,11 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Container();
