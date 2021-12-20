5afdf9cd7dc2a1ee108a68e3fffa50d7c56fea98
Simone Auriemma <simone.auriemma@live.com>
2021-12-20 23:54:25 +0100

    bug fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pet360/model/interface_model.dart';
+import 'package:pet360/model/trainer_model.dart';
+import 'package:pet360/model/user_model.dart';
+import 'package:pet360/model/veterinary_model.dart';
 import 'package:pet360/screens/chat_screen.dart';
 import 'package:pet360/screens/find_fiends.dart';
 import 'package:pet360/screens/navigator_add.dart';
@@ -22,21 +27,35 @@ class _HomeScreenState extends State<HomeScreen> {
   final PageStorageBucket bucket = PageStorageBucket();
   Widget currentScreen = Dashboard();
   final _auth = FirebaseAuth.instance;
+  Future<InterfaceModel>? futureUser;
+
   var jsonBody;
+
+  /*@override
+  void initState() {
+    super.initState();
+    final uid = _auth.currentUser!.uid;
+    if (UserSharedPreferences.getTypeOfUser() == "" ||
+        UserSharedPreferences.getTypeOfUser() == null) {
+      getData("Utente", uid.toString(), "");
+      /*if (jsonBody == null) {
+        getData("Addestratore", uid.toString(), "");
+      }
+      if (jsonBody == null) {
+        getData("Veterinario", uid.toString(), "");
+      }*/
+    }
+  }*/
 
   @override
   void initState() {
     super.initState();
     final uid = _auth.currentUser!.uid;
-    if (UserSharedPreferences.getTypeOfUser() == "") {
-      getData("Utente", uid.toString(), "");
-      if (jsonBody == null) {
-        getData("Addestratore", uid.toString(), "");
-      }
-      if (jsonBody == null) {
-        getData("Veterinario", uid.toString(), "");
-      }
-    }
+    //print("test INIT");
+    print("typeOfUser..." + UserSharedPreferences.getTypeOfUser().toString());
+    //print(uid);
+    futureUser =
+        fetchUser(UserSharedPreferences.getTypeOfUser().toString(), uid, "");
   }
 
   int index = 0;
@@ -49,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
     ProfileScreen(),
   ];
 
-  @override
+  /* @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
@@ -95,7 +114,69 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: screens[index],
     );
-  }
+  }*/
+
+  @override
+  Widget build(BuildContext context) => FutureBuilder<InterfaceModel>(
+        future: futureUser,
+        builder: (context, snapshot) {
+          //print("Snap: " + snapshot.toString() + jsonBody.toString());
+          if (snapshot.hasData) {
+            return Scaffold(
+              extendBody: true,
+              bottomNavigationBar: CurvedNavigationBar(
+                // color: Color.fromRGBO(197, 225, 165, 5),
+                color: Colors.lightGreen.shade200,
+                backgroundColor: Colors.transparent,
+                items: <Widget>[
+                  Icon(
+                    Icons.home,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  Icon(
+                    Icons.map,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  Icon(
+                    Icons.add,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  Icon(
+                    Icons.chat,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  Icon(
+                    Icons.account_circle,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                ],
+                animationCurve: Curves.easeInOut,
+                animationDuration: Duration(milliseconds: 600),
+                onTap: (index) {
+                  setState(() {
+                    this.index = index;
+                  });
+                },
+                letIndexChange: (index) => true,
+              ),
+              body: screens[index],
+            );
+          }
+          // We can show the loading view until the data comes back.
+          //debugPrint('Step 1, build loading widget');
+          return SizedBox(
+            height: MediaQuery.of(context).size.height / 1,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      );
 
   //getData("User","uid","surnameName")
   getData(String typeOfUser, String uidUser, String path) async {
@@ -111,13 +192,67 @@ class _HomeScreenState extends State<HomeScreen> {
     var response = await http.get(url);
     if (response.statusCode == 200) {
       jsonBody = json.decode(response.body);
-      if (jsonBody != null) {
-        UserSharedPreferences.setTypeOfUser(typeOfUser);
-        //print("Settato: " + typeOfUser);
-      }
-      //print(jsonBody.toString());
-    } else {
+    }
+    //print("Settato: " + typeOfUser);
+    //print(jsonBody.toString());
+    else {
       print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  Future<InterfaceModel> fetchUser(
+      String typeOfUser, String uidUser, String path) async {
+    //print(UserSharedPreferences.getTypeOfUser() == null);
+    if (UserSharedPreferences.getTypeOfUser() == null ||
+        UserSharedPreferences.getTypeOfUser().toString() == "") {
+      await getData("Utente", uidUser, "");
+      typeOfUser = "Utente";
+      UserSharedPreferences.setTypeOfUser(typeOfUser);
+
+      if (jsonBody == null) {
+        await getData("Addestratore", uidUser, "");
+        typeOfUser = "Addestratore";
+        UserSharedPreferences.setTypeOfUser(typeOfUser);
+      }
+      if (jsonBody == null) {
+        await getData("Veterinario", uidUser, "");
+        typeOfUser = "Veterinario";
+        UserSharedPreferences.setTypeOfUser(typeOfUser);
+      }
+    }
+
+    var url = Uri.parse(
+        "https://pet360-43dfe-default-rtdb.europe-west1.firebasedatabase.app//" +
+            typeOfUser +
+            "//" +
+            uidUser +
+            "//" +
+            path +
+            ".json?");
+    //print(url);
+    final response = await http.get(url);
+    /*print("print->>>>" + json.decode(response.body));
+    print("TypeOfUser->>" + UserSharedPreferences.getTypeOfUser().toString());*/
+
+    if (response.statusCode == 200) {
+      jsonBody = json.decode(response.body);
+      //print(json.decode(response.body));
+      if (json.decode(response.body) == null) {
+        switch (typeOfUser) {
+          case "Utente":
+            return UserModel.fromJson(jsonDecode(response.body));
+          case "Addestratore":
+            return TrainerModel.fromJson(jsonDecode(response.body));
+          case "Veterinario":
+            return VeterinaryModel.fromJson(jsonDecode(response.body));
+          default:
+            return UserModel.fromJson(jsonDecode(response.body));
+        }
+      }
+      return UserModel.fromJson(jsonDecode(response.body));
+    } else {
+      //print("testingggggg");
+      throw Exception('Failed to load album');
     }
   }
 }
