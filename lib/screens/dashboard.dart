333a6fd324829e01b532d11e0434821e63fda9c1
Elio Testa <eliotesta1998@libero.it>
2021-12-28 19:37:27 +0100

    Visualizzazione Custom
    Visualizzazione della lista degli animali sulla home e aggiunta della foto sul db e sulla visualizzazione

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pet360/components/appbackground.dart';
-
+import 'package:pet360/model/interface_model.dart';
+import 'package:pet360/model/trainer_model.dart';
+import 'package:pet360/model/user_model.dart';
+import 'package:pet360/model/veterinary_model.dart';
+import 'package:pet360/model/view_animals_home.dart';
+import 'package:http/http.dart' as http;
+import 'package:pet360/utils/usersharedpreferences.dart';
 import 'ia_screen.dart';
 
 class Dashboard extends StatefulWidget {
@@ -12,231 +22,213 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  @override
-  final List<dynamic> _contacts = [
-    {
-      "name": "Airon",
-      "image": 'assets/images/cane1.png',
-    },
-    {
-      "name": "Billie",
-      "image": 'assets/images/cane2.png',
-    },
-    {
-      "name": "Lilly",
-      "image": 'assets/images/cane3.png',
-    },
-    {
-      "name": "Lucky",
-      "image": 'assets/images/cane4.png',
-    },
-    {
-      "name": "Laila",
-      "image": 'assets/images/cane5.png',
-    },
-  ];
+  Future<List<ViewAnimalsHome>>? futureAnimal;
+  final _auth = FirebaseAuth.instance;
 
   @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return AppBackground(
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                height: size.height * 0.3,
-                child: Stack(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(
-                        left: 40,
-                        right: 40,
-                      ),
-                      height: size.height * 0.3 - 27,
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            "Benvenuto!",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline5!
-                                .copyWith(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                          ),
-                          Spacer(),
-                          Stack(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(30),
-                                    topRight: Radius.circular(30),
-                                    bottomLeft: Radius.circular(30),
-                                    bottomRight: Radius.circular(30),
-                                  ),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      spreadRadius: 5,
-                                      blurRadius: 7,
-                                      offset: Offset(0, 3), // changes position of shadow
-                                    ),
-                                  ],
-                                ),
-                                child:  GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => IAscreen()));
-                                  },
-                                  // Image tapped
-                                  child: Image.asset(
-                                    "assets/icons/ped-id.png",
-                                    width: 55,
-                                    height: 50,
-                                    colorBlendMode: BlendMode.multiply,
-                                  ),
-                                ),
-                              ),
+  void initState() {
+    super.initState();
+    final uid = _auth.currentUser!.uid;
+    futureAnimal =
+        fetchAnimals(UserSharedPreferences.getTypeOfUser().toString(), uid, "Animali");
+  }
 
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              /*const Text(
-                "Lista animali",
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Padding(
-                  padding:
-                  EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 10)),
-              */
-
-              Container(
-                width: MediaQuery.of(context).size.width / 1.1,
-                height: 140,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.1),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _contacts.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        //
-                      },
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        margin: EdgeInsets.only(right: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+  @override
+  Widget build(BuildContext context)
+    => FutureBuilder<List<ViewAnimalsHome>>(
+        future: futureAnimal,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Size size = MediaQuery
+                .of(context)
+                .size;
+            return AppBackground(
+              child: Scaffold(
+                backgroundColor: Colors.transparent,
+                body: Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: size.height * 0.3,
+                        child: Stack(
                           children: [
-                            Image.asset(
-                              _contacts[index]['image'],
-                              width: 60,
+                            Container(
+                              padding: const EdgeInsets.only(
+                                left: 40,
+                                right: 40,
+                              ),
+                              height: size.height * 0.3 - 27,
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Benvenuto!",
+                                    style: Theme
+                                        .of(context)
+                                        .textTheme
+                                        .headline5!
+                                        .copyWith(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Spacer(),
+                                  Stack(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: const BorderRadius.only(
+                                            topLeft: Radius.circular(30),
+                                            topRight: Radius.circular(30),
+                                            bottomLeft: Radius.circular(30),
+                                            bottomRight: Radius.circular(30),
+                                          ),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.grey.withOpacity(0.3),
+                                              spreadRadius: 5,
+                                              blurRadius: 7,
+                                              offset: Offset(
+                                                  0,
+                                                  3), // changes position of shadow
+                                            ),
+                                          ],
+                                        ),
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        IAscreen()));
+                                          },
+                                          // Image tapped
+                                          child: Image.asset(
+                                            "assets/icons/ped-id.png",
+                                            width: 55,
+                                            height: 50,
+                                            colorBlendMode: BlendMode.multiply,
+                                          ),
+                                        ),
+                                      ),
+
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              _contacts[index]["name"],
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600),
-                            )
                           ],
                         ),
                       ),
-                    );
-                  },
+
+                      Container(
+                        width: MediaQuery
+                            .of(context)
+                            .size
+                            .width / 1.1,
+                        height: 140,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.1),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                //
+                              },
+                              child: Container(
+                                width: 100,
+                                height: 100,
+                                margin: EdgeInsets.only(right: 10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.file(
+                                      File(snapshot.data![index].pathImg.toString()),
+                                      width: 60,
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      snapshot.data![index].animalName.toString(),
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Spacer(),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Image.asset(
+                          "assets/storyset/homeimg.png",
+                          height: size.width / 2,
+                          width: size.width / 2,
+                        ),
+                      ),
+                      const Padding(
+                          padding:
+                          EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 80)),
+                    ],
+                  ),
                 ),
               ),
-              Spacer(),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Image.asset(
-                  "assets/storyset/homeimg.png",
-                  height: size.width / 2,
-                  width: size.width / 2,
-                ),
-              ),
-              const Padding(
-                  padding:
-                      EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 80)),
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          }
+          return SizedBox(
+            height: MediaQuery.of(context).size.height / 1,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        });
+
   }
 
-/*Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          height: size.height * 0.3,
-          child: Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(
-                  left: 40,
-                  right: 40,
-                ),
-                height: size.height * 0.3 - 27,
-                decoration: BoxDecoration(
-                    color: Colors.lightGreen.shade200,
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(36),
-                      bottomRight: Radius.circular(36),
-                    )),
-                child: Row(
-                  children: <Widget>[
-                    Text("Benvenuto Utente!",
-                        style: Theme.of(context).textTheme.headline5!.copyWith(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                    Spacer(),
-                    Image.asset("assets/icons/ped-id.png", width: 55, height: 50,),
+  Future<List<ViewAnimalsHome>> fetchAnimals(
+      String typeOfUser, String uidUser, String path) async {
+    var url = Uri.parse(
+        "https://pet360-43dfe-default-rtdb.europe-west1.firebasedatabase.app//" +
+            typeOfUser +
+            "//" +
+            uidUser +
+            "//" +
+            path +
+            ".json?");
+    final response = await http.get(url);
+    List<ViewAnimalsHome> list = List.empty(growable: true);
+    if (response.statusCode == 200) {
+      jsonDecode(response.body).forEach((key, value) {
+        ViewAnimalsHome animal = ViewAnimalsHome();
+        animal.animalName = key;
+        animal.pathImg = value["Libretto"]["animalFoto"];
+        list.add(animal);
+      });
+      return list;
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
 
-                  ],
-                ),
-              )
-            ],
-          ),
-        )
-      ],
-    );
-  } */
-}
